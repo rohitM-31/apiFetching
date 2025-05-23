e8783123ce4442be25d77bed606190187497739a
MokaRohit <21pa1a1274@vishnu.edu.in>
2025-05-23 19:18:36 +0530

    First api fetch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Fetch from './components/Fetch';
+function App() {
+  return (
+    <div className="App">
+      <h1>ROHIT</h1>
+      <Fetch/>
+    </div>
+  );
+}
+
+export default App;
